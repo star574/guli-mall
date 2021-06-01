b0de008790966ccd3cb39e92f632285194a69dd2
codestar <shihengluo574@gmail.com>
2021-06-02 01:12:24 +0800

    spring-cloud-alibaba-2.2与mybatis-plus 3.3.2冲突(无法注册nacos)

---
+++ gulimall-ware/src/main/java/com/lsh/gulimall/ware/dao/UndoLogDao.java
@@ -0,0 +1,17 @@
+package com.lsh.gulimall.ware.dao;
+
+import com.lsh.gulimall.ware.entity.UndoLogEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 
+ * 
+ * @author codestar
+ * @email codestar@gmail.com
+ * @date 2021-06-01 00:36:48
+ */
+@Mapper
+public interface UndoLogDao extends BaseMapper<UndoLogEntity> {
+	
+}
---
+++ gulimall-ware/src/main/java/com/lsh/gulimall/ware/dao/WareSkuDao.java
@@ -0,0 +1,17 @@
+package com.lsh.gulimall.ware.dao;
+
+import com.lsh.gulimall.ware.entity.WareSkuEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 商品库存
+ * 
+ * @author codestar
+ * @email codestar@gmail.com
+ * @date 2021-06-01 00:36:48
+ */
+@Mapper
+public interface WareSkuDao extends BaseMapper<WareSkuEntity> {
+	
+}
