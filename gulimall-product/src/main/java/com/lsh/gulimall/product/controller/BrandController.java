d1f3a37567eb60492938ebe8cecb856291d3fa77
star574 <shihengluo574@gmail.com>
2021-06-12 06:12:02 +0800

    nacos

--- gulimall-product/src/main/java/com/lsh/gulimall/product/controller/BrandController.java
+++ gulimall-product/src/main/java/com/lsh/gulimall/product/controller/BrandController.java
@@ -18,8 +18,6 @@ import com.lsh.gulimall.common.utils.PageUtils;
 import com.lsh.gulimall.common.utils.Query;
 
 
-
-
 /**
  * 品牌
  *
@@ -30,63 +28,63 @@ import com.lsh.gulimall.common.utils.Query;
 @RestController
 @RequestMapping("product/brand")
 public class BrandController {
-    @Autowired
-    private BrandService brandService;
+	@Autowired
+	private BrandService brandService;
 
-    /**
-     * 列表
-     */
-    @RequestMapping("/list")
-    // @RequiresPermissions("product:brand:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = brandService.queryPage(params);
+	/**
+	 * 列表
+	 */
+	@RequestMapping("/list")
+	// @RequiresPermissions("product:brand:list")
+	public R list(@RequestParam Map<String, Object> params) {
+		PageUtils page = brandService.queryPage(params);
 
-        return R.ok().put("page", page);
-    }
+		return R.ok().put("page", page);
+	}
 
 
-    /**
-     * 信息
-     */
-    @RequestMapping("/info/{brandId}")
-    // @RequiresPermissions("product:brand:info")
-    public R info(@PathVariable("brandId") Long brandId){
+	/**
+	 * 信息
+	 */
+	@RequestMapping("/info/{brandId}")
+	// @RequiresPermissions("product:brand:info")
+	public R info(@PathVariable("brandId") Long brandId) {
 		BrandEntity brand = brandService.getById(brandId);
 
-        return R.ok().put("brand", brand);
-    }
+		return R.ok().put("brand", brand);
+	}
 
-    /**
-     * 保存
-     */
-    @RequestMapping("/save")
-    // @RequiresPermissions("product:brand:save")
-    public R save(@RequestBody BrandEntity brand){
+	/**
+	 * 保存
+	 */
+	@RequestMapping("/save")
+	// @RequiresPermissions("product:brand:save")
+	public R save(@RequestBody BrandEntity brand) {
 		brandService.save(brand);
 
-        return R.ok();
-    }
+		return R.ok();
+	}
 
-    /**
-     * 修改
-     */
-    @RequestMapping("/update")
-    // @RequiresPermissions("product:brand:update")
-    public R update(@RequestBody BrandEntity brand){
+	/**
+	 * 修改
+	 */
+	@RequestMapping("/update")
+	// @RequiresPermissions("product:brand:update")
+	public R update(@RequestBody BrandEntity brand) {
 		brandService.updateById(brand);
 
-        return R.ok();
-    }
+		return R.ok();
+	}
 
-    /**
-     * 删除
-     */
-    @RequestMapping("/delete")
-    // @RequiresPermissions("product:brand:delete")
-    public R delete(@RequestBody Long[] brandIds){
+	/**
+	 * 删除
+	 */
+	@RequestMapping("/delete")
+	// @RequiresPermissions("product:brand:delete")
+	public R delete(@RequestBody Long[] brandIds) {
 		brandService.removeByIds(Arrays.asList(brandIds));
 
-        return R.ok();
-    }
+		return R.ok();
+	}
 
 }
