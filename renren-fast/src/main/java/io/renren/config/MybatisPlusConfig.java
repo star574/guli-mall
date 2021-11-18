6b3cb43b24e4cb411d634d879feeb90e05b985b0
star574 <luoshiheng574@gmail.com>
2021-11-18 21:10:21 +0800

    add Jenkinsfile

--- renren-fast/src/main/java/io/renren/config/MybatisPlusConfig.java
+++ renren-fast/src/main/java/io/renren/config/MybatisPlusConfig.java
@@ -1,16 +1,21 @@
 /**
  * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
+ * <p>
  * https://www.renren.io
- *
+ * <p>
  * 版权所有，侵权必究！
  */
 
 package io.renren.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 /**
  * mybatis-plus配置
@@ -18,14 +23,22 @@ import org.springframework.context.annotation.Configuration;
  * @author Mark shihengluo574@gmail.com
  */
 @Configuration
+@Slf4j
 public class MybatisPlusConfig {
 
-    /**
-     * 分页插件
-     */
-    @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
-    }
+	@Autowired
+	DataSource dataSource;
+
+	public void checkJDBC() throws SQLException {
+		log.warn("当前数据源信息" + dataSource.getConnection().getMetaData().getURL());
+	}
+
+	/**
+	 * 分页插件
+	 */
+	@Bean
+	public PaginationInterceptor paginationInterceptor() {
+		return new PaginationInterceptor();
+	}
 
 }
